5aadc401b3fc1f761e7c7c604f7aead8ac19e02d
Rugolo-Simone <simonerugolo07@gmail,com>
2024-06-24 16:11:06 +0200

    Fine pagina Elenco.html, ottimizzazione del codice e inizio pagina Prestiti.html

--- js/Elenco.js
+++ js/Elenco.js
@@ -1,5 +1,11 @@
 window.onload = init
 
+var clickData = 0 //Al primo click ordina in ordine decrescente la data, al secondo in ordine crescente
+var clickNome = 0 //Al primo click ordina in ordine decrescente il nome, al secondo in ordine crescente
+var clickGenere = 0 //Al primo click ordina in ordine decrescente il genere, al secondo in ordine crescente
+
+var sectTabella, cancellaTable, tb, tr, th1, th2, th3
+
 var Elenco = {
     "Il Signore degli Anelli": {
         genere: "Fantasy",
@@ -41,38 +47,50 @@ var Elenco = {
         genere: "Tragedia",
         data: 1600,
     },
+    "Don Chisciotte della Mancia": {
+        genere: "Romanzo",
+        data: 1605,
+    },
+    "Orgoglio e pregiudizio": {
+        genere: "Romanzo",
+        data: 1813,
+    },
+    "Guerra e pace": {
+        genere: "Romanzo storico",
+        data: 1869,
+    },
+    "I fratelli Karamazov": {
+        genere: "Romanzo filosofico",
+        data: 1880,
+    },
+    "Madame Bovary": {
+        genere: "Romanzo",
+        data: 1857,
+    },
+    "L'Ulisse": {
+        genere: "Romanzo modernista",
+        data: 1922,
+    },
+    "Il processo": {
+        genere: "Romanzo",
+        data: 1925,
+    },
+    "Uno, nessuno e centomila": {
+        genere: "Romanzo",
+        data: 1926,
+    },
+    "Harry Potter e la pietra filosofale": {
+        genere: "Fantasy",
+        data: 1997,
+    },
+    "Cento anni di solitudine": {
+        genere: "Romanzo",
+        data: 1967,
+    },
 }
 
 function init(){
-
-    let sectTabella = document.getElementById("sectTabella");
-    sectTabella.className = "section"
-
-    let cancellaTable = document.querySelector("table");
-    if (cancellaTable) {
-        sectTabella.removeChild(cancellaTable);
-    }
-
-    let tb = document.createElement("table");
-
-    let tr = document.createElement("tr");
-
-    let th1 = document.createElement("th");
-    th1.innerHTML = "TITOLO";
-    th1.className = "table"
-    tr.appendChild(th1);
-
-    let th2 = document.createElement("th");
-    th2.innerHTML = "GENERE";
-    th2.className = "table"
-    tr.appendChild(th2);
-
-    let th3 = document.createElement("th");
-    th3.innerHTML = "DATA";
-    th3.className = "table"
-    tr.appendChild(th3);
-
-    tb.appendChild(tr);
+    creaTabella()
 
     for(let libro in Elenco){
 
@@ -99,6 +117,37 @@ function init(){
     console.log(tb)
     tb.className = "tb"
     sectTabella.appendChild(tb);
+}
+
+function creaTabella(){
+    sectTabella = document.getElementById("sectTabella");
+    sectTabella.className = "section"
+
+    cancellaTable = document.querySelector("table");
+    if (cancellaTable) {
+        sectTabella.removeChild(cancellaTable);
+    }
+
+    tb = document.createElement("table");
+
+    tr = document.createElement("tr");
+
+    th1 = document.createElement("th");
+    th1.innerHTML = "TITOLO";
+    th1.className = "table"
+    tr.appendChild(th1);
+
+    th2 = document.createElement("th");
+    th2.innerHTML = "GENERE";
+    th2.className = "table"
+    tr.appendChild(th2);
+
+    th3 = document.createElement("th");
+    th3.innerHTML = "DATA";
+    th3.className = "table"
+    tr.appendChild(th3);
+
+    tb.appendChild(tr);
 }
 
 function aggiungi(){
@@ -138,12 +187,197 @@ function aggiungi(){
         cancellaTable.innerHTML = ""
 
         console.log(Elenco)
+        alert("✅LIBRO AGGIUNTO CON SUCCESSO!")
         init()
     }
-
-    alert("✅LIBRO AGGIUNTO CON SUCCESSO!")
 }
 
-function ordinaData(){
-    
+function ordinaData() {
+
+    let elencoArray = [];
+    for (let titolo in Elenco) {
+        elencoArray.push({ titolo: titolo, genere: Elenco[titolo].genere, data: Elenco[titolo].data });
+    }
+
+    if(clickData == 0){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].data < elencoArray[j + 1].data) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+    else if(clickData == 1){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].data > elencoArray[j + 1].data) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    creaTabella()
+
+    for (let i = 0; i < elencoArray.length; i++) {
+        let trF = document.createElement("tr");
+
+        let td1F = document.createElement("td");
+        td1F.innerHTML = elencoArray[i].titolo;
+        td1F.className = "table";
+        trF.appendChild(td1F);
+
+        let td2F = document.createElement("td");
+        td2F.innerHTML = elencoArray[i].genere;
+        td2F.className = "table";
+        trF.appendChild(td2F);
+
+        let td3F = document.createElement("td");
+        td3F.innerHTML = elencoArray[i].data;
+        td3F.className = "table";
+        trF.appendChild(td3F);
+
+        tb.appendChild(trF);
+    }
+
+    tb.className = "tb";
+    sectTabella.appendChild(tb);
+
+    if(clickData == 0){
+        clickData = 1
+    }
+    else if(clickData == 1){
+        clickData = 0
+    }
+}
+
+function ordinaNome(){
+    let elencoArray = [];
+    for (let titolo in Elenco) {
+        elencoArray.push({ titolo: titolo, genere: Elenco[titolo].genere, data: Elenco[titolo].data });
+    }
+
+    if(clickNome == 0){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].titolo < elencoArray[j + 1].titolo) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+    else if(clickNome == 1){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].titolo > elencoArray[j + 1].titolo) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    creaTabella()
+
+    for (let i = 0; i < elencoArray.length; i++) {
+        let trF = document.createElement("tr");
+
+        let td1F = document.createElement("td");
+        td1F.innerHTML = elencoArray[i].titolo;
+        td1F.className = "table";
+        trF.appendChild(td1F);
+
+        let td2F = document.createElement("td");
+        td2F.innerHTML = elencoArray[i].genere;
+        td2F.className = "table";
+        trF.appendChild(td2F);
+
+        let td3F = document.createElement("td");
+        td3F.innerHTML = elencoArray[i].data;
+        td3F.className = "table";
+        trF.appendChild(td3F);
+
+        tb.appendChild(trF);
+    }
+
+    tb.className = "tb";
+    sectTabella.appendChild(tb);
+
+    if(clickNome == 0){
+        clickNome = 1
+    }
+    else if(clickNome == 1){
+        clickNome = 0
+    }
+}
+
+function ordinaGenere(){
+    let elencoArray = [];
+    for (let titolo in Elenco) {
+        elencoArray.push({ titolo: titolo, genere: Elenco[titolo].genere, data: Elenco[titolo].data });
+    }
+
+    if(clickGenere == 0){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].genere < elencoArray[j + 1].genere) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+    else if(clickGenere == 1){
+        for (let i = 0; i < elencoArray.length - 1; i++) {
+            for (let j = 0; j < elencoArray.length - 1 - i; j++) {
+                if (elencoArray[j].genere > elencoArray[j + 1].genere) {
+                    let temp = elencoArray[j];
+                    elencoArray[j] = elencoArray[j + 1];
+                    elencoArray[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    creaTabella()
+
+    for (let i = 0; i < elencoArray.length; i++) {
+        let trF = document.createElement("tr");
+
+        let td1F = document.createElement("td");
+        td1F.innerHTML = elencoArray[i].titolo;
+        td1F.className = "table";
+        trF.appendChild(td1F);
+
+        let td2F = document.createElement("td");
+        td2F.innerHTML = elencoArray[i].genere;
+        td2F.className = "table";
+        trF.appendChild(td2F);
+
+        let td3F = document.createElement("td");
+        td3F.innerHTML = elencoArray[i].data;
+        td3F.className = "table";
+        trF.appendChild(td3F);
+
+        tb.appendChild(trF);
+    }
+
+    tb.className = "tb";
+    sectTabella.appendChild(tb);
+
+    if(clickGenere == 0){
+        clickGenere = 1
+    }
+    else if(clickGenere == 1){
+        clickGenere = 0
+    }
 }
